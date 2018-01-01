43ff8507d3aea40fd470c8e9455cdba02c29e018
Samhitha Jagannath <samhitha.kutsa@gmail.com>
2018-01-01 20:58:45 +0530

    TechCrunch.js component

---
+++ client/src/TechCrunch.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Tumblr extends Component {
+  constructor(props) {
+    
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+  
+  }
+
+export default TechCrunch;
+
+
