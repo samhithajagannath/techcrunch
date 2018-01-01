1337c0b5dc8ffc4df4228295f96f93145da906df
Samhitha Jagannath <samhitha.kutsa@gmail.com>
2018-01-01 20:59:56 +0530

    feed api call

--- client/src/TechCrunch.js
+++ client/src/TechCrunch.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import CardComponent from './CardComponent'
 
 class Tumblr extends Component {
   constructor(props) {
-    
+    super(props)
+    this.state = {
+      requestFailed: false,
+      }
   }
 
   componentDidMount() {
-    
+    var myHeaders = new Headers();
+    fetch('/feed')
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed")
+        }
+        return response
+      })
+      .then(d => d.json())
+      .then(d => {
+        console.log(d);
+        this.setState({
+          githubData: d
+        })
+      }, () => {
+        this.setState({
+          requestFailed: true
+        })
+    })
   }
 
   render() {
-  
+    if (this.state.requestFailed) return <p>Failed!</p>
+    if (!this.state.githubData) return <p>Loading...</p>
   }
 
 export default TechCrunch;
