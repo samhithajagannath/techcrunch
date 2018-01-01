3a7452e40b6d38aef2120b51f7712e837f0f8715
Samhitha Jagannath <samhitha.kutsa@gmail.com>
2018-01-01 21:04:42 +0530

    add muitheme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TechCrunch from './TechCrunch.js'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
