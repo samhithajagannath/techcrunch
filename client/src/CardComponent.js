8e6cc47b27076af0b389998086e0f4df2a426d03
Samhitha Jagannath <samhitha.kutsa@gmail.com>
2018-02-02 01:06:39 +0530

    Added like Button

--- client/src/CardComponent.js
+++ client/src/CardComponent.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-const CardComponent = (props) => (
-  <div>
-    <Card style={{"width":"30%"}}>
-      <CardHeader
-        title={props.data.source_name}
-        subtitle={props.data.date}
-      />
-      <CardMedia
-        overlay={<CardTitle title={props.data.title} subtitle={props.data.author} />}
-      >
-        <img src={props.data.image} alt=""/>
-      </CardMedia>
-      <CardTitle title={props.data.title} subtitle={props.data.url} />
-
-      <p>
-        {props.data.content}
-      </p>
-      <CardActions>
-        <FlatButton label="Article Source" href={props.data.url}/>
-      </CardActions>
-    </Card>
-    <br/>
-  </div>
-);
 
 
-function clickhandler(url){
-  return window.open(url);
+class CardComponent extends Component {
+
+    constructor(props) {
+    super(props)
+    this.state = {
+      likecount: 0,
+    }
+  }
+
+  likeClicked = () => {
+    this.setState({
+      likecount: this.state.likecount + 1
+    })
+    return;
+  }
+
+ 
+  render() {
+
+    return (
+    <div>
+      <Card style={{"width":"30%"}}>
+        <CardHeader
+          title={this.props.data.source_name}
+          subtitle={this.props.data.date}
+        />
+        <CardMedia
+          overlay={<CardTitle title={this.props.data.title} subtitle={this.props.data.author} />}
+        >
+          <img src={this.props.data.image} alt=""/>
+        </CardMedia>
+        <CardTitle title={this.props.data.title} subtitle={this.props.data.url} />
+
+        <p>
+          {this.props.data.content}
+        </p>
+        <CardActions>
+          <FlatButton label="Article Source" href={this.props.data.url}/>
+        </CardActions>
+        <CardActions>
+          <button onClick={this.likeClicked.bind(this)}> Like </button>
+          <span> {this.state.likecount} </span>
+        </CardActions>
+      </Card>
+      <br/>
+    </div>
+  )
+  }
 }
 export default CardComponent;
